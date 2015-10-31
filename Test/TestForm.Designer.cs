31f71dc99fa2056f9a945429ba4d94dbb17e01ea
unknown <D:\TheBat!>
2015-10-31 23:18:54 +0200

    Додано пакетні файли до індексу

---
+++ Test/TestForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Test
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid1 = new SourceGrid.Grid();
+            this.SuspendLayout();
+            // 
+            // grid1
+            // 
+            this.grid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid1.EnableSort = true;
+            this.grid1.Location = new System.Drawing.Point(0, 0);
+            this.grid1.Name = "grid1";
+            this.grid1.OptimizeMode = SourceGrid.CellOptimizeMode.ForRows;
+            this.grid1.SelectionMode = SourceGrid.GridSelectionMode.Cell;
+            this.grid1.Size = new System.Drawing.Size(656, 498);
+            this.grid1.TabIndex = 0;
+            this.grid1.TabStop = true;
+            this.grid1.ToolTipText = "";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 498);
+            this.Controls.Add(this.grid1);
+            this.Name = "TestForm";
+            this.Text = "TestForm";
+            this.Load += new System.EventHandler(this.TestForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SourceGrid.Grid grid1;
+    }
+}
+
---
+++ Test/TestForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Victors;
+
+namespace Test
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            FBClient client = new FBClient();
+            DataTable dt = client.QueryRecordsList(@"select * from orders");
+            List<dynamic> param = new List<dynamic>
+            {
+                "Найменування", "orderno",
+                "Дата готовності", "dateorder"
+            };
+            Dictionary dict = new Dictionary(param);
+            SourceGridUtilities.Fill(grid1, dt, dict);
+        }
+    }
+}
